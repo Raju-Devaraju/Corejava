c2f086483910e8d500617aa814562bc6f71362a7
Raju-Devaraju <rajudevaraju1810@gmail.com>
2026-07-04 13:45:57 +0530

    comment added

--- src/com/rd/Programming/EvenOddDigitsofNumber.java
+++ src/com/rd/Programming/EvenOddDigitsofNumber.java
@@ -13,7 +13,7 @@ public class EvenOddDigitsofNumber {
 		int even_count=0;
 		int odd_count=0;
 		
-		while(num>0) {
+		while(num>0) {//this is my comment
 			int rem=num%10;
 			if(rem%2==0) {
 				even_count++;
